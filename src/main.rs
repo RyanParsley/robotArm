444e808485023efdcbdb1a9775abf138e3723855
Ryan Parsley <RMParsley@gmail.com>
2024-03-08 08:18:15 -0500

    chore: tidy up comments

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ use embassy_time::{ Duration, Timer};
 use panic_probe as _;
 use defmt::info;
 
-// TODO: research all of these
+//This macro defines the right interrupt handlers, and creates a unit struct (like struct Irqs;) and implements the right Bindings for it.
+//You can pass this struct to drivers to prove at compile-time that the right interrupts have been bound. 
 bind_interrupts!(struct Irqs {
     ADC_IRQ_FIFO => InterruptHandler;
 });
@@ -20,12 +21,17 @@ bind_interrupts!(struct Irqs {
 async fn main(_spawner: Spawner) {
     info!("Initializing...");
 
-    // I get that this is of type Peripherals, but what is it?
     let peripherals = embassy_rp::init(Default::default());
 
     let mut adc = Adc::new(peripherals.ADC, Irqs, Config::default());
 
-    // Motors 18, 19, 20 & 21
+    // Notes on servos
+    // Since just about every GPIO on the Pico is appropriate for PWM, I can put the servos
+    // anywhere. I plan to connect motors to the following pins 18, 19, 20 & 21 because it's a
+    // block of 4 that fits between 2 GNDs on the side of the board that happens to be near the
+    // robot as my breadboard is mounted. That is to say, no great reason for this choice. 
+ 
+    // Notes on Potentiometers and the Pico.
     // Potentiometers need to be on ADC Ports so 26, 27 and 28 are the only appropriate pins.
     // ADC_VREF and AGND are special power and ground for working with ADC. 
 
@@ -34,9 +40,11 @@ async fn main(_spawner: Spawner) {
     info!("Initialized.");
 
     loop {
-        // When I use the 100k potentiometer, this unwrap fails gloriously.
+        // TODO: When I use the 100k potentiometer, this unwrap fails gloriously
+        // [issue](https://github.com/RyanParsley/robotArm/issues/1).
         // What's the right way to handle that?
         // Why does it happen?
+        //
         // ERROR panicked at src/main.rs:37:47:
         // called `Result::unwrap()` on an `Err` value: ConversionFailed
 
