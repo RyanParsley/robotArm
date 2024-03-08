d0d785b60a15852e82b536b134a13566ae589318
Ryan Parsley <RMParsley@gmail.com>
2024-03-07 21:40:34 -0500

    feat: hook up a potentiometer to get my feet wet
    More than a blinking hello world, but less than a full blown robot,
    driving the servos with potentiometers feels like a sensible checkpoint.
    
    In this commit, I learned how to hook one up and realized a Pico only
    supports up to 3 because you need a special ADC enabled pin. One servo
    per motor won't be a thing, but one for the pincher, one for the base
    rotation still seems worth doing.
    
    Not sure if or how I'd use the third without getting into kinematics.
    Maybe I'll just swap between 3 of 4 motors fiddling with code.
    
    Things Learned:
    ---------------
    - Learn about ADC (analog to digital converters) to read potentiometer
      resistance as digital values.
    - Establish a workflow deploying code from the commandline using the
      debug probe.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
 #![no_std]
+// Why do I get an error about "can't find crate for `test`?" on no_std?
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_rp::gpio::{Level, Output};
-use embassy_time::{Duration, Timer};
+use embassy_rp::gpio::Pull;
+use embassy_rp::adc::{Adc, Channel, Config, InterruptHandler};
+use embassy_rp::bind_interrupts;
+use embassy_time::{ Duration, Timer};
 use panic_probe as _;
+use defmt::info;
+
+// TODO: research all of these
+bind_interrupts!(struct Irqs {
+    ADC_IRQ_FIFO => InterruptHandler;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    defmt::info!("Initializing...");
+    info!("Initializing...");
 
+    // I get that this is of type Peripherals, but what is it?
     let peripherals = embassy_rp::init(Default::default());
-    let mut led = Output::new(peripherals.PIN_25, Level::Low);
 
-    defmt::info!("Initialized.");
+    let mut adc = Adc::new(peripherals.ADC, Irqs, Config::default());
+
+    // Motors 18, 19, 20 & 21
+    // Potentiometers need to be on ADC Ports so 26, 27 and 28 are the only appropriate pins.
+    // ADC_VREF and AGND are special power and ground for working with ADC. 
+
+    let mut pot1 = Channel::new_pin(peripherals.PIN_26, Pull::None);
+
+    info!("Initialized.");
 
     loop {
-        defmt::info!("LED on!");
-        led.set_high();
-        Timer::after(Duration::from_millis(50)).await;
+        // When I use the 100k potentiometer, this unwrap fails gloriously.
+        // What's the right way to handle that?
+        // Why does it happen?
+        // ERROR panicked at src/main.rs:37:47:
+        // called `Result::unwrap()` on an `Err` value: ConversionFailed
 
-        defmt::info!("LED off!");
-        led.set_low();
-        Timer::after(Duration::from_millis(950)).await;
+        let level = adc.read(&mut pot1).await.unwrap();
+        info!("Pot1 ADC: {}", level);
+
+        Timer::after(Duration::from_millis(1000)).await;
     }
 }
