8193e7361522cc6622adc516adef601392794a21
Ryan Parsley <RMParsley@gmail.com>
2024-03-31 21:25:25 -0400

    feat: use pwm on an led successfully

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,25 @@
 #![no_std]
 #![no_main]
-#![feature(type_alias_impl_trait)]
 
-use defmt::info;
-use defmt_rtt as _;
+use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::adc::{Adc, Channel, Config, InterruptHandler};
-use embassy_rp::bind_interrupts;
-use embassy_rp::gpio::Pull;
-use embassy_time::{Duration, Timer};
-use panic_probe as _;
-
-/*
-* This macro defines the right interrupt handlers, creates a unit struct and implements the right Bindings for it.
-* You can pass this struct to drivers to prove at compile-time that the right interrupts have been bound.
-*/
-bind_interrupts!(struct Irqs {
-    ADC_IRQ_FIFO => InterruptHandler;
-});
+use embassy_rp::pwm::{Config, Pwm};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    info!("Initializing...");
+    let p = embassy_rp::init(Default::default());
 
-    let peripherals = embassy_rp::init(Default::default());
-
-    let mut adc = Adc::new(peripherals.ADC, Irqs, Config::default());
-
-    /* ---------------
-     * Notes on servos
-     * ---------------
-     * Since just about every GPIO on the Pico is appropriate for PWM, I can put the servos
-     * anywhere. I plan to connect motors to the following pins 18, 19, 20 & 21 because it's a
-     * block of 4 that fits between 2 GNDs on the side of the board that happens to be near the
-     * robot as my breadboard is mounted. That is to say, no great reason for this choice.
-     */
-
-    /* ------------------------------------
-     * Notes on Potentiometers and the Pico
-     * ------------------------------------
-     * Potentiometers need to be on ADC Ports so 26, 27 and 28 are the only appropriate pins.
-     *
-     * DON'T USE ADC_VREF!
-     * Use the standard 3v3 pin for power.
-     */
-
-    let mut pot1 = Channel::new_pin(peripherals.PIN_26, Pull::None);
-
-    info!("Initialized.");
+    let mut c: Config = Default::default();
+    c.top = 0x8000;
+    c.compare_b = 8;
+    let mut pwm = Pwm::new_output_b(p.PWM_CH7, p.PIN_15, c.clone());
 
     loop {
-        let level = adc.read(&mut pot1).await.unwrap();
-        info!("Pot1 ADC: {:?}", level);
-
-        Timer::after(Duration::from_millis(1000)).await;
+        info!("current LED duty cycle: {}/32768", c.compare_b);
+        Timer::after_secs(1).await;
+        c.compare_b = c.compare_b.rotate_left(4);
+        pwm.set_config(&c);
     }
 }
